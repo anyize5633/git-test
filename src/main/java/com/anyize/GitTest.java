4869941ac632dd2ed878342c6601f686b93e1274
anyize <anyize@at2740598447.com>
2022-06-03 18:23:52 +0800

    first commit

--- src/main/java/com/anyize/GitTest.java
+++ src/main/java/com/anyize/GitTest.java
@@ -3,5 +3,6 @@ package com.anyize;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello giti");
+        System.out.println("hello git3");
     }
 }
