685eeb1dd1ee47d2e90a9249058b7aed48569ce1
anyize <anyize@at2740598447.com>
2022-06-03 18:36:32 +0800

    second commit

--- src/main/java/com/anyize/GitTest.java
+++ src/main/java/com/anyize/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello 4665656i");
         System.out.println("hello g4455it3");
+        System.out.println("hello 1234566");
     }
 }
