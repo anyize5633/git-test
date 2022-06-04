9c0ff730b1926bdc1fdbddd3b1736af4883357db
anyize <anyize@at2740598447.com>
2022-06-04 09:02:35 +0800

    hot-fix commit3

--- src/main/java/com/anyize/GitTest.java
+++ src/main/java/com/anyize/GitTest.java
@@ -2,7 +2,7 @@ package com.anyize;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello 4665656i");
+        System.out.println("456");
         System.out.println("hello g4455it3");
         System.out.println("hello 1234566");
         System.out.println("woshihot-fix");
